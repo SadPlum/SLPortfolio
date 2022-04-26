d8964338e2b78ee0ecdf9d1fef8f030e67a75c4d
SadPlum <92168542+SadPlum@users.noreply.github.com>
2022-04-26 20:18:45 -0300

    push all

---
+++ src/Hooks/useIntersectionAnimation.ts
@@ -0,0 +1,29 @@
+import { useEffect } from "react";
+
+const useIntersectionAnimation = (
+  reference: React.RefObject<HTMLElement>,
+  changeState: (arg: boolean) => void,
+  options?: {
+    root?: HTMLElement;
+    rootMargin?: string;
+    threshold?: number;
+  }
+) => {
+  const observerCallback = (entries: any) => {
+    const entry = entries[0];
+    if (entry.isIntersecting) {
+      changeState(true);
+      console.log(true);
+    }
+  };
+  useEffect(() => {
+    let observer = new IntersectionObserver(observerCallback, options);
+    let watched: any = reference.current;
+    if (reference) observer.observe(watched);
+    return () => {
+      if (reference) observer.unobserve(watched);
+    };
+  }, [reference]);
+};
+
+export default useIntersectionAnimation;
---
+++ src/Hooks/useIntersectionChangeTab.ts
@@ -0,0 +1,32 @@
+import { useEffect } from "react";
+
+const useIntersectionObserverChangeTab = (
+  reference: React.RefObject<HTMLElement>,
+  changeTab: (title: string) => void,
+  title: string,
+  options?: {
+    root?: HTMLElement;
+    rootMargin?: string;
+    threshold?: number;
+  }
+) => {
+  const observerCallback = (entries: any) => {
+    const entry = entries[0];
+    if (entry.isIntersecting) {
+      changeTab(title);
+    }
+    if (entry.intersectionRatio >= 0.3) {
+      changeTab(title);
+    }
+  };
+
+  useEffect(() => {
+    let observer = new IntersectionObserver(observerCallback, options);
+    let watched: any = reference.current;
+    if (reference) observer.observe(watched);
+    return () => {
+      if (reference) observer.unobserve(watched);
+    };
+  }, [reference]);
+};
+export default useIntersectionObserverChangeTab;
